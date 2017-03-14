7b4e16610b680b759224718346779216bacc276b
Unknown <spirem@gmail.com>
2017-03-14 10:49:50 +0100

    clean up

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,17 +1,16 @@
 package controller;
 
+import socket.ISocketController;
 import socket.SocketController;
 import weight.IWeightInterfaceController;
 import weight.gui.WeightGUI;
-import socket.ISocketController;
 /**
  * Simple class to fire up application and inject implementations
  * @author Christian
  *
  */
 public class Main {
-	private static boolean gui= true;
-
+	
 	public static void main(String[] args) {
 		ISocketController socketHandler = new SocketController();
 		IWeightInterfaceController weightController = new WeightGUI();
@@ -19,6 +18,5 @@ public class Main {
 		IMainController mainCtrl = new MainController(socketHandler, weightController);
 		//.init and .start could be merged
 		mainCtrl.start();
-		
 	}
 }
